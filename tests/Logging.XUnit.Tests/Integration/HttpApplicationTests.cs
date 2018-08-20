c83d15591df4b5b31f2b40ee43d9b67cf8d628d5
martincostello <martin@martincostello.com>
2018-08-20 18:18:50 +0100

    Add sample app to test accessor
    Add a sample application to test usage of ITestOutputHelperAccessor with integration tests for an ASP.NET Core application.

---
+++ tests/Logging.XUnit.Tests/Integration/HttpApplicationTests.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Martin Costello, 2018. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MartinCostello.Logging.XUnit.Integration
+{
+    [Collection(HttpServerCollection.Name)]
+    public sealed class HttpApplicationTests : IDisposable
+    {
+        public HttpApplicationTests(HttpServerFixture fixture, ITestOutputHelper outputHelper)
+        {
+            Fixture = fixture;
+            Fixture.SetOutputHelper(outputHelper);
+        }
+
+        private HttpServerFixture Fixture { get; }
+
+        public void Dispose()
+        {
+            Fixture.ClearOutputHelper();
+        }
+
+        [Fact]
+        public async Task Http_Get_Many()
+        {
+            // Arrange
+            using (var httpClient = Fixture.CreateClient())
+            {
+                // Act
+                using (var response = await httpClient.GetAsync("api/values"))
+                {
+                    // Assert
+                    response.IsSuccessStatusCode.ShouldBeTrue();
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Http_Get_Single()
+        {
+            // Arrange
+            using (var httpClient = Fixture.CreateClient())
+            {
+                // Act
+                using (var response = await httpClient.GetAsync("api/values/a"))
+                {
+                    // Assert
+                    response.IsSuccessStatusCode.ShouldBeTrue();
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Http_Post()
+        {
+            // Arrange
+            using (var httpClient = Fixture.CreateClient())
+            {
+                // Act
+                using (var response = await httpClient.PostAsJsonAsync("api/values", "d"))
+                {
+                    // Assert
+                    response.IsSuccessStatusCode.ShouldBeTrue();
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Http_Put()
+        {
+            // Arrange
+            using (var httpClient = Fixture.CreateClient())
+            {
+                // Act
+                using (var response = await httpClient.PutAsJsonAsync("api/values/d", "d"))
+                {
+                    // Assert
+                    response.IsSuccessStatusCode.ShouldBeTrue();
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Http_Delete()
+        {
+            // Arrange
+            using (var httpClient = Fixture.CreateClient())
+            {
+                // Act
+                using (var response = await httpClient.DeleteAsync("api/values/d"))
+                {
+                    // Assert
+                    response.IsSuccessStatusCode.ShouldBeTrue();
+                }
+            }
+        }
+    }
+}
---
+++ tests/Logging.XUnit.Tests/Integration/HttpServerFixture.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Martin Costello, 2018. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SampleApp;
+using Xunit.Abstractions;
+
+namespace MartinCostello.Logging.XUnit.Integration
+{
+    /// <summary>
+    /// A test fixture representing an HTTP server hosting the sample application. This class cannot be inherited.
+    /// </summary>
+    public sealed class HttpServerFixture : WebApplicationFactory<Startup>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpServerFixture"/> class.
+        /// </summary>
+        public HttpServerFixture()
+            : base()
+        {
+            // HACK Force HTTP server startup
+            using (CreateDefaultClient())
+            {
+            }
+        }
+
+        /// <summary>
+        /// Clears the current <see cref="ITestOutputHelper"/>.
+        /// </summary>
+        public void ClearOutputHelper()
+        {
+            Server.Host.Services.GetRequiredService<ITestOutputHelperAccessor>().OutputHelper = null;
+        }
+
+        /// <summary>
+        /// Sets the <see cref="ITestOutputHelper"/> to use.
+        /// </summary>
+        /// <param name="value">The <see cref="ITestOutputHelper"/> to use.</param>
+        public void SetOutputHelper(ITestOutputHelper value)
+        {
+            Server.Host.Services.GetRequiredService<ITestOutputHelperAccessor>().OutputHelper = value;
+        }
+
+        /// <inheritdoc />
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureLogging((p) => p.AddXUnit());
+        }
+    }
+}
