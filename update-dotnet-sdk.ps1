ccf0ed686e49a3e55b716da20686b1c06c921e13
martincostello <martin@martincostello.com>
2020-05-19 19:45:39 +0100

    Fix SDK update fix
    Pick up fixes for updating the .NET Core SDK.

--- update-dotnet-sdk.ps1
+++ update-dotnet-sdk.ps1
@@ -122,7 +122,7 @@ function Get-Latest-SDK-Version([string] $FileName) {
     return $Version
 }
 
-function Get-Latest-Runtime-Version([string] $FileName, [string] $SdkVersion) {
+function Get-Latest-Runtime-Version([string] $FileName, [string] $SdkVersion, [bool] $AllowNull = $false) {
 
     if (-Not (Test-Path $FileName)) {
         throw "Unable to find '$FileName'"
@@ -142,21 +142,36 @@ function Get-Latest-Runtime-Version([string] $FileName, [string] $SdkVersion) {
         Say-Verbose "JSON: $JsonContent"
 
         try {
-            $JsonContent | ForEach-Object {
+            foreach ($_ in $JsonContent) {
                 if ($_.sdk.version -eq $SdkVersion) {
                     $Version = $_.sdk."runtime-version"
+                    break;
                 }
             }
         }
         catch {
-            throw "Unable to parse the releases node in '$FileName'"
+            throw "Unable to parse the releases sdk node in '$FileName'"
+        }
+
+        if ($null -eq $Version) {
+            try {
+                foreach ($_ in $JsonContent) {
+                    if ($_.sdks.version -eq $SdkVersion) {
+                        $Version = $_.sdks."runtime-version" | Select -First 1
+                        break;
+                    }
+                }
+            }
+            catch {
+                throw "Unable to parse the releases sdks node in '$FileName'"
+            }
         }
     }
     else {
         throw "Unable to find the releases node in '$FileName'"
     }
 
-    if ($null -eq $Version) {
+    if (($null -eq $Version) -And ($AllowNull -eq $false)) {
         throw "Unable to find the releases node in '$FileName' for SDK version $SdkVersion"
     }
 
@@ -210,7 +225,14 @@ finally {
 
 $LatestSDKVersion = Get-Latest-SDK-Version $ReleaseNotesPath
 $LatestRuntimeVersion = Get-Latest-Runtime-Version $ReleaseNotesPath $LatestSDKVersion
-$CurrentRuntimeVersion = Get-Latest-Runtime-Version $ReleaseNotesPath $CurrentSDKVersion
+$CurrentRuntimeVersion = Get-Latest-Runtime-Version $ReleaseNotesPath $CurrentSDKVersion -AllowNull ($LatestSDKVersion -lt $CurrentSDKVersion)
+
+if ($null -eq $CurrentRuntimeVersion) {
+    Say "Unable to determine runtime version for .NET SDK version $CurrentSDKVersion."
+    return
+}
+
+Say-Verbose "Current .NET runtime version is $CurrentRuntimeVersion"
 
 Say "Latest .NET SDK version for channel '$Channel' is $LatestSDKVersion (runtime version $LatestRuntimeVersion)"
 
