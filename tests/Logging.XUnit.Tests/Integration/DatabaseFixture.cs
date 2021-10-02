095a649903f023288e925ddc53e62076efaf4c60
Cédric Luthi <cedric.luthi@gmail.com>
2021-10-02 15:17:30 +0200

    Tweak logger names

--- tests/Logging.XUnit.Tests/Integration/DatabaseFixture.cs
+++ tests/Logging.XUnit.Tests/Integration/DatabaseFixture.cs
@@ -11,29 +11,29 @@ namespace MartinCostello.Logging.XUnit.Integration
 {
     public sealed class DatabaseFixture : IAsyncLifetime
     {
-        private readonly ILogger _loggerInitialize;
-        private readonly ILogger _loggerDispose;
+        private readonly ILogger _initializeLogger;
+        private readonly ILogger _disposeLogger;
         private string? _connectionString;
 
         public DatabaseFixture(IMessageSink messageSink)
         {
             using var loggerFactory = new LoggerFactory();
-            _loggerInitialize = loggerFactory.AddXUnit(messageSink, c => c.MessageSinkMessageFactory = m => new PrintableDiagnosticMessage(m)).CreateLogger<DatabaseFixture>();
-            _loggerDispose = messageSink.ToLogger<DatabaseFixture>();
+            _initializeLogger = loggerFactory.AddXUnit(messageSink, c => c.MessageSinkMessageFactory = m => new PrintableDiagnosticMessage(m)).CreateLogger<DatabaseFixture>();
+            _disposeLogger = messageSink.ToLogger<DatabaseFixture>();
         }
 
         public string ConnectionString => _connectionString ?? throw new InvalidOperationException("The connection string is only available after InitializeAsync has completed.");
 
         Task IAsyncLifetime.InitializeAsync()
         {
-            _loggerInitialize.LogInformation("Initializing database");
+            _initializeLogger.LogInformation("Initializing database");
             _connectionString = "Server=localhost";
             return Task.CompletedTask;
         }
 
         Task IAsyncLifetime.DisposeAsync()
         {
-            _loggerDispose.LogInformation("Disposing database");
+            _disposeLogger.LogInformation("Disposing database");
             return Task.CompletedTask;
         }
     }
