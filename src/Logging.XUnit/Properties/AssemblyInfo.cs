aa507a92e33f9bb9260649dc64822f96f1425420
martincostello <martin@martincostello.com>
2023-10-25 19:17:44 +0100

    Use MSBuild for assembly attributes
    Remove `CommonAssemblyInfo.cs` and use MSBuild to set assembly attributes.

--- src/Logging.XUnit/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Martin Costello, 2018. All rights reserved.
-// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
-
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: Guid("af808007-f06a-410b-886d-152b3f39c43f")]
-
-[assembly: InternalsVisibleTo("MartinCostello.Logging.XUnit.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001004b0b2efbada897147aa03d2076278890aefe2f8023562336d206ec8a719b06e89461c31b43abec615918d509158629f93385930c030494509e418bf396d69ce7dbe0b5b2db1a81543ab42777cb98210677fed69dbeb3237492a7ad69e87a1911ed20eb2d7c300238dc6f6403e3d04a1351c5cb369de4e022b18fbec70f7d21ed")]
