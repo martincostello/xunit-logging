87fc351b79912a7931a46cc5595bf232a292fb72
martincostello <martin@martincostello.com>
2025-09-12 16:11:46 +0100

    Simplify parameter validation
    Use C# 14 extension-everything to polyfill `ArgumentNullException.ThrowIfNull()`.

---
+++ src/Shared/ArgumentNullExceptionExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Martin Costello, 2018. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+#if NETFRAMEWORK || NETSTANDARD
+using System.Runtime.CompilerServices;
+
+#pragma warning disable IDE0130
+namespace System;
+
+/// <summary>
+/// Extension methods for <see cref="ArgumentNullException"/>.
+/// </summary>
+internal static class ArgumentNullExceptionExtensions
+{
+    extension(ArgumentNullException)
+    {
+        /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
+        /// <param name="argument">The reference type argument to validate as non-null.</param>
+        /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+        public static void ThrowIfNull(object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument is null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
+#endif
---
+++ src/Shared/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Martin Costello, 2018. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+#if NETFRAMEWORK || NETSTANDARD
+#pragma warning disable IDE0130
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Indicates that a parameter captures the expression passed for another parameter as a string.
+/// </summary>
+/// <param name="parameterName">The name of the parameter whose expression should be captured as a string.</param>
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+internal sealed class CallerArgumentExpressionAttribute(string parameterName) : Attribute
+{
+    /// <summary>
+    /// Gets the name of the parameter whose expression should be captured as a string.
+    /// </summary>
+    public string ParameterName { get; } = parameterName;
+}
+#endif
